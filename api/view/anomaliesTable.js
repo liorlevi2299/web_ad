4d129b4ef90c37f26f9e156d532c49564b2b7754
Lior Levi <lior.levi5@live.biu.ac.il>
2021-05-12 18:16:14 +0300

    changing the view of the table to a beautiful table:)

--- api/view/anomaliesTable.js
+++ api/view/anomaliesTable.js
@@ -18,8 +18,6 @@ function updateTable(){
         }
     }
 
-
-
     let i = 1;
     numOfRows = 0;
     anomaliesList.forEach((value, key)=>{
@@ -29,7 +27,23 @@ function updateTable(){
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
         cell1.innerHTML = key;
-        cell2.innerHTML = value.toString();
+        //cell2.innerHTML = value.toString();
+        cell2.innerHTML = '[' + value[0].toString();
+        for(let v = 0; v < value.length; v++) {
+            if(v === value.length - 1 && value[v - 1] + 1 === value[v]) {
+                // last anomaly in array, and the continuation of the last set
+                cell2.innerHTML += '-' + value[v].toString() + ']';
+            } else if(v === value.length - 1) {
+                // last anomaly in array, and a new set
+                cell2.innerHTML += '[' + value[v].toString() + ']';
+            }
+            else if(value[v] + 1 === value[v+1]) {
+                // continue
+            } else {
+                cell2.innerHTML += '-' + value[v].toString() + ']';
+                cell2.innerHTML += ', [' + value[v + 1].toString();
+            }
+        }
         numOfRows++
     })
 }
